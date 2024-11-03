7f375834310a1190da6c99dcbcfa05f6eb096f24
stiven <johan.1701922249@ucaldas.edu.co>
2024-11-02 22:13:17 -0500

    form appointment

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppointmentComponent } from '../patient/components/appointment/appointment.component';
 
 
+const routes: Routes = [
+  { path: 'agendar-cita', component: AppointmentComponent },
+  // Otras rutas de tu aplicación
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '**', redirectTo: '/inicio' }
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    [RouterModule.forRoot(routes)],
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/patient/components/appointment/appointment.component.ts
+++ src/app/patient/components/appointment/appointment.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment',
-  standalone: true,
-  imports: [],
   templateUrl: './appointment.component.html',
-  styleUrl: './appointment.component.css'
+  styleUrls: ['./appointment.component.css']
 })
-export class AppointmentComponent {
 
+export class AppointmentComponent {
+  appointmentForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.appointmentForm = this.fb.group({
+      patientName: ['', Validators.required],
+      appointmentDate: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      speciality: ['', Validators.required],
+      notes: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.appointmentForm.valid) {
+      console.log('Appointment Data:', this.appointmentForm.value);
+      // Aquí iría la lógica para enviar los datos al backend
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
