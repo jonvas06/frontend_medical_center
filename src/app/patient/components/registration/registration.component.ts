9441b37f64e57196598155c02e029b4c9e5af423
Sebastian Gallego <112526670+SebastianGallego02@users.noreply.github.com>
2024-11-03 20:26:54 -0500

    Se hace formulario básico (no funcional) para el registro de pacientes

--- src/app/patient/components/registration/registration.component.ts
+++ src/app/patient/components/registration/registration.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'register-patient',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
+export class RegistrationComponent{
+  registrationForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      patientFName: ['', Validators.required],
+      patientSName: ['', Validators.required],
+      patientFSame: ['', Validators.required],
+      patientSSame: ['', Validators.required],
+      patientemail: ['', Validators.required],
+      patientdirec: ['', Validators.required],
+      patientphone: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log('Datos del formulario:', this.registrationForm.value);
+      // Aquí puedes añadir lógica para enviar los datos al backend
+    }
+  }
 }
