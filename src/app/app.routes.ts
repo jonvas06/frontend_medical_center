9339d3e7eb9571c292904867e166fec8c9a3ff2f
Sebastian Gallego <112526670+SebastianGallego02@users.noreply.github.com>
2024-12-10 17:46:21 -0500

    Se agrega el rol de administrador para todo el sistema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './Guards/auth.guard';
 
 export const routes: Routes = [
-    {path: 'register-patient', component: PatientRegistrationComponent, canActivate: [AuthGuard], data: { roles: [2] } },
-    {path: 'register-doctor', component: DoctorRegistrationComponent, canActivate: [AuthGuard], data: { roles: [2] }},
+    {path: 'register-patient', component: PatientRegistrationComponent, canActivate: [AuthGuard], data: { roles: [2,3] } },
+    {path: 'register-doctor', component: DoctorRegistrationComponent, canActivate: [AuthGuard], data: { roles: [2,3] }},
     {path: 'login', component: LoginComponent},
-    {path: 'agendar-cita', component: AppointmentComponent, canActivate: [AuthGuard], data: { roles: [1] }},
+    {path: 'agendar-cita', component: AppointmentComponent, canActivate: [AuthGuard], data: { roles: [1,3] }},
     {path: '**', redirectTo: '', pathMatch: 'full'}
 
 ];
