b2f16e6bb5a7fb7c194b869ebed19444863bd2ec
Sebastian Gallego <112526670+SebastianGallego02@users.noreply.github.com>
2024-12-09 23:44:30 -0500

    fase inicial de validacion de roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { PatientRegistrationComponent } from './patient/components/registration/
 import { AppointmentComponent } from './patient/components/appointment/appointment.component';
 import { DoctorRegistrationComponent } from './doctor/components/registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './Guards/auth.guard';
 
 export const routes: Routes = [
-    {path: 'register-patient', component: PatientRegistrationComponent},
+    {path: 'register-patient', component: PatientRegistrationComponent, canActivate: [AuthGuard]},
     {path: 'register-doctor', component: DoctorRegistrationComponent},
     {path: 'login', component: LoginComponent},
     {path: 'agendar-cita', component: AppointmentComponent},
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,11 +11,21 @@ import { Observable } from 'rxjs';
 
 export class AuthService {
   private apiUrl = 'http://localhost:3000/api/user/login';
+  private isAuthenticated = false;
 
   constructor(private http: HttpClient) {}
 
   login(credentials: { email: string; password: string }): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.isAuthenticated = true;
     return this.http.post(this.apiUrl, credentials, { headers });
+  }
+
+  logout() {
+    this.isAuthenticated = false;
+  }
+
+  isLoggedIn(): boolean {
+    return this.isAuthenticated;
   }
 }
