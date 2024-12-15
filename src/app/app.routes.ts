3d066241a75c41bb5e8fef05a4cb78ff0d26b837
Sebastian Gallego <112526670+SebastianGallego02@users.noreply.github.com>
2024-12-15 11:17:43 -0500

    se agregan las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { AppointmentComponent } from './patient/components/appointment/appointme
 import { DoctorRegistrationComponent } from './doctor/components/registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './Guards/auth.guard';
+import { PatientListComponent } from './patient/components/patient-list/patient-list.component';
+import { DoctorListComponent } from './doctor/components/doctor-list/doctor-list.component';
 
 export const routes: Routes = [
     {path: 'register-patient', component: PatientRegistrationComponent, canActivate: [AuthGuard], data: { roles: [2,3] } },
+    {path: 'patient-list', component: PatientListComponent, canActivate: [AuthGuard], data: { roles: [3] } },
     {path: 'register-doctor', component: DoctorRegistrationComponent, canActivate: [AuthGuard], data: { roles: [2,3] }},
+    {path: 'doctor-list', component: DoctorListComponent, canActivate: [AuthGuard], data: { roles: [3] } },
     {path: 'login', component: LoginComponent},
     {path: 'agendar-cita', component: AppointmentComponent, canActivate: [AuthGuard], data: { roles: [1,3] }},
     {path: '**', redirectTo: '', pathMatch: 'full'}
