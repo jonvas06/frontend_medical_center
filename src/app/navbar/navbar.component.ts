202e744eba0493d5442c50f1d9e6ac7c9a289a24
stiven <johan.1701922249@ucaldas.edu.co>
2024-12-16 15:27:15 -0500

    agregue pagina responsive, nav y inicio

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { PatientRegistrationComponent } from '../patient/components/registration/registration.component'
+import { Component, ViewChild, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DoctorRegistrationComponent } from '../doctor/components/registration/registration.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterLink, RouterOutlet],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
 
+  @ViewChild('menuToggle', { static: false }) menuToggle!: ElementRef;
+  @ViewChild('navbarLinks', { static: false }) navbarLinks!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    // Verificamos que los elementos existen antes de agregar el listener
+    if (this.menuToggle && this.navbarLinks) {
+      this.renderer.listen(this.menuToggle.nativeElement, 'click', () => {
+        this.navbarLinks.nativeElement.classList.toggle('active');
+      });
+    }
+  }
 }
