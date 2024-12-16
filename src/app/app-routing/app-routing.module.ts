dd78b73de179d25818ad28dd01fc10229091ab27
stiven <johan.1701922249@ucaldas.edu.co>
2024-12-16 14:33:00 -0500

    fix routing contacto y servicios

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,11 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppointmentComponent } from '../patient/components/appointment/appointment.component';
 import { PatientRegistrationComponent } from '../patient/components/registration/registration.component';
 import { DoctorRegistrationComponent } from '../doctor/components/registration/registration.component';
+import { ContactoComponent } from '../otherComponent/contacto/contacto.component';
+import { ServiciosComponent } from '../otherComponent/servicios/servicios.component';
 
 const routes: Routes = [
   { path: 'agendar-cita', component: AppointmentComponent },
   { path: 'register-patient', component: PatientRegistrationComponent },
   { path: 'register-doctor', component: DoctorRegistrationComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'servicios', component: ServiciosComponent },
   // Otras rutas aplicación
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: '', redirectTo: '/Admin', pathMatch: 'full' },
